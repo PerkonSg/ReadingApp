88f4e25a371b16f01148be688ba007dfe18e14ab
Maximilian Ang <191439L@student.hci.edu.sg>
2019-11-02 12:32:31 +0800

    zheng yi gay

--- ReadingApp/ReadyViewController.swift
+++ ReadingApp/ReadyViewController.swift
@@ -9,15 +9,40 @@
 import UIKit
 
 class ReadyViewController: UIViewController {
-
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var numLabel: UILabel!
+    
     var designatedTitle = ""
+    var count = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         title = designatedTitle
+        numLabel.text = "\(count)"
+        label1.text = "Quiz on \(designatedTitle) starting in"
+        var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(update),userInfo: nil, repeats: true)
+    
+    
+
+
+        
+        
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    @objc func update() {
+        if(count > 1) {
+            count -= 1
+            numLabel.text = "\(count)"
+        }
+        else{
+            numLabel.text = ""
+            label1.text = "BEGIN!"
+        }
+    }
+    
     
 
     /*
